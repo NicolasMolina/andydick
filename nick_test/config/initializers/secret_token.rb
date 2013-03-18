9ee44337ab251b6dbbaa0363abcaf354d7e95e7b
Nicolas Molina <nicolasmolina@Nicolass-MacBook-Pro.local>
2013-03-18 08:10:47 +0200

    1stcommit

---
+++ nick_test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NickTest::Application.config.secret_token = 'cb06c9a1aac9c8b5aa29f2d1c9eb4825e392d95033b3aa0c146bd4614ba17883260e2ba82426301a38d412179ad55af930eec091316193a0a8f90ffa800c2e85'
